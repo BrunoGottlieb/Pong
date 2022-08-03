6bb91478fc98f3bc663e44aef3fdc5351d1c670a
BrunoGottlieb <48501861+BrunoGottlieb@users.noreply.github.com>
2022-08-02 22:33:04 -0300

    Add: Icon and PS5 joystick support

--- Assets/_Project/Scripts/InputManager.cs
+++ Assets/_Project/Scripts/InputManager.cs
@@ -6,27 +6,27 @@ namespace _Project.Scripts
     {
         public static bool MoveUp()
         {
-            return Input.GetAxis("Vertical") > 0.1f;
+            return Input.GetAxis("Vertical") > 0.1f || Input.GetAxis("JoystickAxis") > 0.1f;
         }
     
         public static bool MoveDown()
         {
-            return Input.GetAxis("Vertical") < -0.1f;
+            return Input.GetAxis("Vertical") < -0.1f || Input.GetAxis("JoystickAxis") < -0.1f;
         }
 
         public static bool Restart()
         {
-            return Input.GetKeyDown(KeyCode.R);
+            return Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.JoystickButton1);
         }
         
         public static bool Pause()
         {
-            return Input.GetKeyDown(KeyCode.P);
+            return Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.JoystickButton3);
         }
         
         public static bool Escape()
         {
-            return Input.GetKeyDown(KeyCode.Escape);
+            return Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.JoystickButton2);
         }
     }
 }
